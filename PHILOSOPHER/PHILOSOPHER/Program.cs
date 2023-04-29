4f51f0923284278cd66aef5ecd1f118c6e749bbe
mahmood2002reda <111697516+mahmood2002reda@users.noreply.github.com>
2023-04-29 22:29:36 +0200

    Add files via upload

---
+++ PHILOSOPHER/PHILOSOPHER/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static int numPhilosophers = 5;
+    static object[] forks = new object[numPhilosophers];
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < numPhilosophers; i++)
+        {
+            forks[i] = new object();
+        }
+
+        for (int i = 0; i < numPhilosophers; i++)
+        {
+            int philosopherNumber = i;
+            Thread thread = new Thread(() => Eat(philosopherNumber));
+            thread.Start();
+        }
+
+        Console.ReadLine();
+    }
+
+    static void Eat(int philosopherNumber)
+    {
+        int leftFork = philosopherNumber;
+        int rightFork = (philosopherNumber + 1) % numPhilosophers;
+
+        lock (forks[leftFork])
+        {
+            lock (forks[rightFork])
+            {
+                Console.WriteLine("Philosopher " + philosopherNumber + " is eating");
+            }
+        }
+    }
+}
